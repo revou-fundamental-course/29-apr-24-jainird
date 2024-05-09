07ab28d574ddca149ca6953c74e77b9693e8c428
Muhammad Jaini Rasyid <mjainird@gmail.com>
2024-05-09 21:55:06 +0800

    initial configuration

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
 // Ini JavaScript
+
+function validation() {
+    console.log('Good');
+}
+
+document.getElementById('home-btn').addEventListener('click', () => validation())
+
+let currentIndex = 0;
+  const slides = document.querySelectorAll('.carousel-item');
+  const totalSlides = slides.length;
+  const intervalTime = 3000;
+
+  function moveSlide(direction) {
+    currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+    updateCarousel();
+  }
+
+  function updateCarousel() {
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+  }
+
+ 
+  setInterval(() => {
+    moveSlide(1);
+  }, intervalTime);
+
+
